f6735ffbe5e966cdf662bdd8629452364af72bc1
ShineDecade <lamjicopass@gmail.com>
2023-12-01 00:25:13 +0700

    BaiThi

--- Đề 2011/Bai2_de2011.cpp
+++ Đề 2011/Bai2_de2011.cpp
@@ -1,31 +1,62 @@
-#ifndef _PhanSo_h
-#define _PhanSo_h
-
 #include <iostream>
+using namespace std;
 
-class PhanSo {
+class MayLoc {
 private:
-    int tu, mau;
-
+    float m_tgian;
 public:
-    // Constructors
-    PhanSo();
-    PhanSo(int tu, int mau);
-
-    // Phép toán cộng
-    PhanSo operator+(const PhanSo& other) const;
-
-    // Phép toán +=
-    PhanSo& operator+=(const PhanSo& other);
-
-    // Phép toán ++ (tiền tố)
-    PhanSo& operator++();
-
-    // Phép gán từ int
-    PhanSo& operator=(int value);
-
-    // Xuất phân số ra màn hình
-    friend std::ostream& operator<<(std::ostream& os, const PhanSo& ps);
+    void setTgian(float tgian) {
+        m_tgian = tgian;
+    }
+    float getThoiGian() {
+        return m_tgian;
+    }
 };
 
-#endif
+class MayXucTac : public MayLoc {
+private:
+    int _luongHoaChat;
+    float _congSuatLoc;
+public:
+    void nhap() {
+        float thoiGian = 0;
+        cout << "Nhap thoi gian hoat dong:";
+        cin >> thoiGian;
+        this->setTgian(thoiGian);
+        cout << "Nhap Luong hoa chat:";
+        cin >> this->_luongHoaChat;
+        cout << "Nhap cong suat loc cua may:";
+        cin >> this->_congSuatLoc;
+    }
+    float CongSuatThucTe() {
+        if (this->getThoiGian() >= 10) {
+            return this->_congSuatLoc * (this->_luongHoaChat / 100) / (this->getThoiGian() / 10);
+        }
+        else {
+            return this->_congSuatLoc * (this->_luongHoaChat / 100);
+        }
+    }
+    static long DON_GIA_HOA_CHAT;
+    static long DON_GIA_THUE;
+    long tinhChoPhiThueMoiMay() {
+        return DON_GIA_THUE * this->getThoiGian();
+    }
+    long tinhChiPhi() {
+        return tinhChoPhiThueMoiMay() + this->_luongHoaChat * 10000;
+    }
+    float tinhLuongNuoc() {
+        return this->getThoiGian() * this->CongSuatThucTe();
+    }
+};
+
+long MayXucTac::DON_GIA_HOA_CHAT = 10000;
+long MayXucTac::DON_GIA_THUE = 80000;
+
+int main() {
+    cout << " Don gia hoa chat la " << MayXucTac::DON_GIA_HOA_CHAT << endl;
+    MayXucTac m;
+    m.nhap();
+    cout << "Chi phi su dung may: " << m.tinhChiPhi() << endl;
+    cout << "Luong nuoc loc duoc: " << m.tinhLuongNuoc() << endl;
+    return 0;
+}
--- Đề 2011/Bai3b_de2011.cpp
+++ Đề 2011/Bai3b_de2011.cpp
@@ -1,31 +1,93 @@
-#ifndef _PhanSo_h
-#define _PhanSo_h
-
 #include <iostream>
+#include <vector>
 
-class PhanSo {
-private:
-    int tu, mau;
-
+class MayLoc {
 public:
-    // Constructors
-    PhanSo();
-    PhanSo(int tu, int mau);
-
-    // Phép toán cộng
-    PhanSo operator+(const PhanSo& other) const;
-
-    // Phép toán +=
-    PhanSo& operator+=(const PhanSo& other);
-
-    // Phép toán ++ (tiền tố)
-    PhanSo& operator++();
-
-    // Phép gán từ int
-    PhanSo& operator=(int value);
-
-    // Xuất phân số ra màn hình
-    friend std::ostream& operator<<(std::ostream& os, const PhanSo& ps);
+    virtual double TinhCongSuatLoc() const = 0;
+    virtual double TinhChiPhi() const = 0;
 };
 
-#endif
+class MayLyTam : public MayLoc {
+private:
+    const double donGia = 50000;  // Đơn giá thuê
+    const double chiPhiNhienLieu = 20000;  // Chi phí nhiên liệu/vật liệu
+
+public:
+    double TinhCongSuatLoc() const override {
+        return 0;  // Cần thay đổi để tính công suất thực tế
+    }
+
+    double TinhChiPhi() const override {
+        return chiPhiNhienLieu * TinhCongSuatLoc();
+    }
+};
+
+class MayXucTac : public MayLoc {
+private:
+    const double donGia = 80000;  // Đơn giá thuê
+    const double chiPhiHoaChat = 10000;  // Chi phí hóa chất/vật liệu
+
+    double thoiGianSuDungHoaChat;
+
+public:
+    double TinhCongSuatLoc() const override {
+        return 0;  // Cần thay đổi để tính công suất thực tế
+    }
+
+    double TinhChiPhi() const override {
+        return chiPhiHoaChat * thoiGianSuDungHoaChat;
+    }
+
+    void NhapThoiGianSuDungHoaChat() {
+        std::cout << "Nhap thoi gian su dung hoa chat (gio): ";
+        std::cin >> thoiGianSuDungHoaChat;
+    }
+};
+
+int main() {
+    int N;
+    std::cout << "Nhap so luong may loc: ";
+    std::cin >> N;
+
+    std::vector<MayLoc*> danhSachMayLoc;
+    for (int i = 0; i < N; ++i) {
+        char loaiMay;
+        std::cout << "Nhap loai may (L/X): ";
+        std::cin >> loaiMay;
+
+        MayLoc* mayLoc = nullptr;
+        if (loaiMay == 'L') {
+            mayLoc = new MayLyTam();
+        } else if (loaiMay == 'X') {
+            mayLoc = new MayXucTac();
+            dynamic_cast<MayXucTac*>(mayLoc)->NhapThoiGianSuDungHoaChat();
+        } else {
+            std::cerr << "Loai may khong hop le.\n";
+            return 1;
+        }
+
+        danhSachMayLoc.push_back(mayLoc);
+    }
+
+    double luongNuoc;
+    std::cout << "Nhap luong nuoc trong ao (m3): ";
+    std::cin >> luongNuoc;
+
+    double tongCongSuatLoc = 0;
+    double tongChiPhi = 0;
+
+    for (const auto& mayLoc : danhSachMayLoc) {
+        tongCongSuatLoc += mayLoc->TinhCongSuatLoc();
+        tongChiPhi += mayLoc->TinhChiPhi();
+    }
+
+    std::cout << "Tong cong suat loc: " << tongCongSuatLoc << " (thuoc don vi chua thay doi)\n";
+    std::cout << "Tong chi phi su dung may loc: " << tongChiPhi << " dong\n";
+
+    // Giải phóng bộ nhớ
+    for (const auto& mayLoc : danhSachMayLoc) {
+        delete mayLoc;
+    }
+
+    return 0;
+}
